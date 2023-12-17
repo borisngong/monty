7b26097b3ee1f2b514f8e654c9351d64fde1885e
borisngong <borisngong@gmail>
2023-12-17 15:34:00 -0800

    The monty program

--- check_is_comment.c
+++ check_is_comment.c
@@ -1,17 +1,18 @@
 #include "monty.h"
 
 /**
- * is_comment - responsible for checking if a substring of the token
+ * check_is_comment - responsible for checking if a substring of the token
  * starts with "#"
  * Return: 0 or 1
  */
 int check_is_comment(void)
 {
-    if (arg_data->substring && arg_data->substring[0] &&
-        strncmp(arg_data->substring[0], "#", 1) == 0)
-    {
-        return 1;
-    }
+	if (
+		arg_data->substring &&
+		arg_data->substring[0] &&
+		arg_data->substring[0][0] == '#'
+	)
+		return (1);
 
-    return 0;
+	return (0);
 }
